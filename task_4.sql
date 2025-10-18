e6d374e3fb18f046f8135debca77ac4dd4d65f64
AnastaseNtez <anastasentezi@gmail.com>
2025-10-18 19:49:25 +0200

    describe a table without using describe or explain keywords

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,13 @@
--- task_4.sql
--- This script prints the full description of the table 'books' without using DESCRIBE or EXPLAIN
-
+-- Script to print the full description of the books table
 SELECT 
-    TABLE_NAME,
     COLUMN_NAME,
     COLUMN_TYPE,
     IS_NULLABLE,
-    COLUMN_DEFAULT,
     COLUMN_KEY,
+    COLUMN_DEFAULT,
     EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-  AND TABLE_NAME = 'books';
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
