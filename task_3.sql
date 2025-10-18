5bb300167d12158238bd666ecc37fb8dca52007f
AnastaseNtez <anastasentezi@gmail.com>
2025-10-18 19:25:08 +0200

    adding a script to retrieve all tables in a database

---
+++ task_3.sql
@@ -0,0 +1,5 @@
+-- SQL script to list all tables in the database specified at runtime.
+-- When running the mysql command, the database name (e.g., alx_book_store)
+-- should be passed as an argument, making the USE command optional.
+
+SHOW TABLES;
