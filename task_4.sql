b79f7ea99d0e7db0eb71b1a6f25877d74de3f85f
AnastaseNtez <anastasentezi@gmail.com>
2025-10-18 19:36:48 +0200

    persistent bug

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,3 @@
 -- SQL script to print the full description (CREATE statement) of the 'Books' table.
 -- This script prints the full description of the table 'books' without using DESCRIBE or EXPLAIN
-
-SELECT 
-    TABLE_NAME,
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_DEFAULT,
-    COLUMN_KEY,
-    EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-  AND TABLE_NAME = 'books';
+SHOW CREATE books;
