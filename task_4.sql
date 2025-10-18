1b6f4416a52ff931387cd441aae842f477cab5bb
AnastaseNtez <anastasentezi@gmail.com>
2025-10-18 19:31:27 +0200

    add a script to print description of a table

---
+++ task_4.sql
@@ -0,0 +1,4 @@
+-- SQL script to print the full description (CREATE statement) of the 'Books' table.
+-- This command is used to display the table structure when DESCRIBE or EXPLAIN are disallowed.
+
+SHOW CREATE TABLE BOOKS;
